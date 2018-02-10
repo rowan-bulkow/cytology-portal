76c2f283b6153274e09c6781692fd1c25a83648b
Joshua Nugent <joshua.nugent@yahoo.com>
2018-02-10 10:11:09 -0900

    initial application creation

---
+++ cytology-portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cytology_portal.MvcApplication" Language="C#" %>
---
+++ cytology-portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cytology_portal.Startup))]
+namespace cytology_portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
