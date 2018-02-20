30c5981ee7f5cfe3461df8e3e4044352b48a5052
Joshua Nugent <joshua.nugent@yahoo.com>
2018-02-19 21:46:59 -0900

    added ability to select image and segment it

--- cytology-portal/Controllers/HomeController.cs
+++ cytology-portal/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using System.Drawing;
+using ImageMagick;
+using OpenCvSharp;
 
 namespace cytology_portal.Controllers
 {
@@ -11,7 +14,13 @@ namespace cytology_portal.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            cytology_portal.Models.CytologyViewModel model = new cytology_portal.Models.CytologyViewModel();
+            string projectDirectory = Path.GetDirectoryName(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            string[] files = Directory.GetFiles(projectDirectory + "/Content/Images/");
+            files.ToList();
+            model.ImageNames = new SelectList(files);
+            
+            return View(model);
         }
 
         public ActionResult About()
@@ -27,12 +36,14 @@ namespace cytology_portal.Controllers
 
             return View();
         }
-
+         
         [HttpPost]
-        public ActionResult SaveImage(HttpPostedFileBase file)
+        public ActionResult SaveImage(HttpPostedFileBase file, cytology_portal.Models.CytologyViewModel model)
         {
-            if (ModelState.IsValid && file != null)
+            if (ModelState.IsValid && file != null && file.ContentType.StartsWith("image"))
             {
+                string test = file.ContentType;
+                string test2 = file.ContentType.FirstOrDefault().ToString();
                 var originalFilename = Path.GetFileName(file?.FileName);
                 //string fileId = Guid.NewGuid().ToString().Replace("-", "");
                 //fileId = originalFilename + fileId;
@@ -42,6 +53,52 @@ namespace cytology_portal.Controllers
                 return RedirectToAction("Index");
             }
             return RedirectToAction("Index"); 
+        }
+         
+        public ActionResult SegmentImage(cytology_portal.Models.CytologyViewModel model)
+        {
+            //          myBitmap = new Bitmap(@"C:\Documents and   
+            //Settings\Joe\Pics\myPic.bmp");
+            //         Graphics g = Graphics.FromImage(myBitmap);
+            string projectDirectory = Path.GetDirectoryName(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            //Image img = Image.FromFile(projectDirectory + "/Content/Images/image3.png");
+            //Graphics g = Graphics.FromImage(img);
+            //g.DrawImage(img, 500, 500);
+            var filename = Path.GetFileName(model.Filename);
+            var path = Path.Combine(Server.MapPath("~/Content/Images/"), "Seg" + filename);
+            //Image img = Image.FromFile(projectDirectory + "/Content/Images/" + filename);
+            //img.Save(path);
+            using (MagickImage image = new MagickImage(model.Filename))
+            {
+                
+                //image.RePage();
+                image.Crop(model.X, model.Y, model.Width, model.Height);
+                image.Segment(ColorSpace.Lab, model.ClusteringThreshold, model.SmoothingThreshold);
+                var fileinfo = new FileInfo(path);
+                image.Write(fileinfo);
+                //var originalFilename = Path.GetFileName(model.Filename);
+                //var path = Path.Combine(Server.MapPath("~/Content/Images/"), originalFilename);
+            }
+            //using (Mat image = new Mat(projectDirectory + "/Content/Images/image3.png"))
+            //{
+            //    using (Window window = new Window(image))
+            //    {
+            //        Window.WaitKey();
+            //    }
+            //}
+
+
+            //string[] files = Directory.GetFiles(projectDirectory + "/Content/Images/");
+            //foreach(var s in files)
+            //{
+            //    var name = Path.GetFileName(s.ToString());
+            //}
+            //Image.FromFile
+            //    System.IO.i
+
+
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ cytology-portal/Models/CytologyViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace cytology_portal.Models
+{
+    public class CytologyViewModel
+    {
+        public double SmoothingThreshold { get; set; }
+        public double ClusteringThreshold { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Filename { get; set; }
+        public SelectList ImageNames { get; set; } 
+    }
+}
