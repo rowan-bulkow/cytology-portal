16e1151dc3ab261149f9043e8ea4119e690e9a11
rowan-bulkow <rowan.bulkow@gmail.com>
2018-02-27 15:16:36 -0900

    Working implementation of vl quickshift algorithm

--- analyzer/segment.cpp
+++ analyzer/segment.cpp
@@ -40,117 +40,49 @@ int main (int argc, const char * argv[]) {
 
     double* vlimg = (double*) toFloat.data;
 
-    int kernelsize = 5;
-    int maxdist = 10;
+    int kernelsize = 2;
+    int maxdist = 6;
 
     int channels = 3;
     int width = image.cols;
     int height = image.rows;
     printf("Image data: (rows) %i (cols) %i (channels) %i\n", height, width, channels);
 
-    printf("Beginning quickshift...");
+    printf("Beginning quickshift...\n");
 
     VlQS* quickshift = vl_quickshift_new(vlimg, width, height, channels);
     vl_quickshift_set_kernel_size(quickshift, kernelsize);
     vl_quickshift_set_max_dist(quickshift, maxdist);
     vl_quickshift_process(quickshift);
     int* parents = vl_quickshift_get_parents(quickshift);
-
-    for(int i=0; ; i++)
-    {
-        if(parents[i] == '\0')
-        {
-            printf("Found the last index of parents at:%i\n", i);
-            break;
-        }
-    }
-    for(int i=0; ; i++)
-    {
-        if(vlimg[i] == '\0')
-        {
-            printf("Found the last index of vlimg at:%i\n", i);
-            break;
-        }
-    }
-
-    double* subset = (double*) calloc(sizeof(double), channels*width*height);
-    for(int c=0; c<channels; c++)
-    {
-        for(int col=0; col<width; col++)
-        {
-            for(int row=0; row<height; row++)
-            {
-                int linearIndex = row + col*height + c*width*height;
-                subset[linearIndex] = vlimg[linearIndex];
-                if(row < 10 || col < 10)
-                    subset[linearIndex] = 0.5;
-                if(linearIndex%3 == 2)
-                    subset[linearIndex] = 0.5;
-            }
-        }
-    }
-    Mat img_subset = Mat(height, width, CV_64FC3, subset);
-    imshow("subset", img_subset);
-
-    int maxval = 0;
+    double* dists = vl_quickshift_get_dists(quickshift);
+    int superpixelcount = 0;
     for(int i=0; i<width*height; i++)
     {
-        if(parents[i] > maxval)
-            maxval = parents[i];
+        if(dists[i] > maxdist)
+            superpixelcount++;
     }
-    printf("Max parent index found:%i\n", maxval);
-
-    for(int c=0; c<channels; c++)
-    {
-        for(int col=0; col<width; col++)
-        {
-            for(int row=0; row<height; row++)
-            {
-                int linearIndex = row + col*height + c*width*height;
-                if(linearIndex%3 == 0)
-                {
-                    int parentIndex = parents[col + row*height] + c*width*height;
-                    vlimg[linearIndex] = vlimg[parentIndex];
-                    vlimg[linearIndex+1] = vlimg[parentIndex+1];
-                    vlimg[linearIndex+2] = vlimg[parentIndex+2];
-                }
-                //
-                //
-                // vlimg[row + col*height + c*width*height] = vlimg[parents[row + col*height] + c*width*height];
-            }
-        }
-    }
+    printf("superpixelcount=%i\n", superpixelcount);
 
     for(int col=0; col<width; col++)
     {
         for(int row=0; row<height; row++)
         {
-            if(row < 10 && col < 10)
-                printf("(%i)%i\t", row+col*height, parents[row + col*height]);
-            else if(row == 10 && col < 10)
-                printf("\n");
-            else if(row > 10 && col > 10)
-                break;
+            int linearIndex = row + col*height;
+            int parentIndex = parents[linearIndex];
+            while(true)
+            {
+                if(dists[parentIndex] > maxdist)
+                    break;
+                parentIndex = parents[parentIndex];
+            }
+
+            for(int c=0; c<channels; c++)
+            {
+                vlimg[linearIndex*3 + c] = vlimg[parentIndex*3 + c];
+            }
         }
     }
-    Mat parentImg = Mat(height, width, CV_32S, parents);
-    imshow("parentImg", parentImg);
-
-    // maxval = 0;
-    // for(int i=0; i < width*height; i++)
-    // {
-    //     int row = i%height;
-    //     int col = floor(i/height);
-    //     for(int c=0; c<channels; c++)
-    //     {
-    //         int linearIndex = c*width*height + row + col*height;
-    //         linearIndex = row + col*height;
-    //         vlimg[linearIndex] = vlimg[c*width*height + parents[i]];
-    //         if(linearIndex > maxval)
-    //             maxval = linearIndex;
-    //     }
-    // }
-    // printf("Max index found:%i\n", maxval);
 
     Mat postQuickShift = Mat(quickshift->height, quickshift->width, CV_64FC3, vlimg);
     imshow("quickshifted", postQuickShift);
