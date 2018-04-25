5fe5f8ddb7a6b4bce50fdbaf09054cdbfa34eacf
rowan-bulkow <rowan.bulkow@gmail.com>
2018-04-24 17:32:17 -0800

    formatting

--- analyzer/segment.cpp
+++ analyzer/segment.cpp
@@ -18,9 +18,8 @@ export LD_LIBRARY_PATH=/usr/local/lib/vlfeat-0.9.21/bin/glnxa64
 (or append this to the existing LD_LIBRARY_PATH if needed)
 
 then run the executable and cross your fingers
-*/
 
-/*
+For command line arguments:
 how to use reference: https://theboostcpplibraries.com/boost.program_options
 get boost on linux: https://stackoverflow.com/questions/12578499/how-to-install-boost-on-ubuntu
 		/use the apt-get version
@@ -39,36 +38,58 @@ using namespace boost::program_options;
 
 int main (int argc, const char * argv[])
 {
+    // Default parameters
+    // Quickshift params
+    int kernelsize = 2;
+    int maxdist = 4;
+    // Canny params
+    int threshold1 = 20;
+    int threshold2 = 40;
+    // GMM params
+    int maxGmmIterations = 10;
+    // GMM post processing params
+    double minAreaThreshold = 200.0;
+    // MSER params
+    int delta = 8, minArea = 15, maxArea = 100;
+    double maxVariation = 0.5, minDiversity = 0.25;
+
     try
     {
         options_description desc{"Options"};
         desc.add_options()
           ("help,h", "Help screen")
-          ("maxVariation", value<float>()->default_value(0.5f), "MaxVariation")
-          ("minDiversity", value<float>()->default_value(0.25f), "MinDiversity")
-          ("minAreaThreshold", value<float>()->default_value(50.0f), "minAreaThreshold")
-          ("kernelsize", value<int>()->default_value(2), "Kernelsize")
-          ("maxdist", value<int>()->default_value(4), "Maxdist")
-          ("threshold1", value<int>()->default_value(20), "threshold1")
-          ("threshold2", value<int>()->default_value(40), "threshold2")
-          ("maxGmmIterations", value<int>()->default_value(10), "maxGmmIterations")
-          ("delta", value<int>()->default_value(0), "delta")
-          ("minArea", value<int>()->default_value(15), "minArea")
-          ("maxArea", value<int>()->default_value(100), "maxArea")
-          ("photo", value<std::string>()->default_value("cyto.tif"), "photo");
+          ("maxVariation", value<float>()->default_value(maxVariation), "MaxVariation")
+          ("minDiversity", value<float>()->default_value(minDiversity), "MinDiversity")
+          ("minAreaThreshold", value<float>()->default_value(minAreaThreshold), "minAreaThreshold")
+          ("kernelsize", value<int>()->default_value(kernelsize), "Kernelsize")
+          ("maxdist", value<int>()->default_value(maxdist), "Maxdist")
+          ("threshold1", value<int>()->default_value(threshold1), "threshold1")
+          ("threshold2", value<int>()->default_value(threshold2), "threshold2")
+          ("maxGmmIterations", value<int>()->default_value(maxGmmIterations), "maxGmmIterations")
+          ("delta", value<int>()->default_value(delta), "delta")
+          ("minArea", value<int>()->default_value(minArea), "minArea")
+          ("maxArea", value<int>()->default_value(maxArea), "maxArea")
+          ("photo,p", value<std::string>()->default_value("cyto.tif"), "photo");
 
         variables_map vm;
         store(parse_command_line(argc, argv, desc), vm);
         notify(vm);
 
+        segment::Segmenter seg = segment::Segmenter(
+            vm["kernelsize"].as<int>(),
+            vm["maxdist"].as<int>(),
+            vm["threshold1"].as<int>(),
+        	vm["threshold2"].as<int>(),
+            vm["maxGmmIterations"].as<int>(),
+            vm["minAreaThreshold"].as<float>(),
+            vm["delta"].as<int>(),
+        	vm["minArea"].as<int>(),
+            vm["maxArea"].as<int>(),
+            vm["maxVariation"].as<float>(),
+            vm["minDiversity"].as<float>()
+        );
 
-        segment::Segmenter seg = segment::Segmenter(vm["kernelsize"].as<int>(), vm["maxdist"].as<int>(), vm["threshold1"].as<int>(),
-        	vm["threshold2"].as<int>(), vm["maxGmmIterations"].as<int>(), vm["minAreaThreshold"].as<float>(), vm["delta"].as<int>(),
-        	vm["minArea"].as<int>(), vm["maxArea"].as<int>(), vm["maxVariation"].as<float>(), vm["minDiversity"].as<float>());
-
-        seg.runSegmentation(vm["photo"].as<std::string>().c_str());
-        // seg.runSegmentation("../images/cyto13.tif");
-        // seg.test();
+        seg.runSegmentation(vm["photo"].as<std::string>());
     }
     catch (const error &ex)
     {
